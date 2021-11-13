19f20bb1496d7b8f5baa0cb6409a14b99015c52a
Parsa <92381546+Dire02@users.noreply.github.com>
2021-11-13 20:44:07 +0330

    Add files via upload

---
+++ register/register.php
@@ -0,0 +1,18 @@
+<?php
+//var_dump( $_POST );
+
+//conect to db
+		$dbc = new mysqli ( 'localhost', 'root', '', 'Users' );
+//insert query
+		$sql = "INSERT INTO users (name,pass,gender,email)
+				VALUES('{$_POST['name']}', '{$_POST['pass']}', '{$_POST['gender']}', '{$_POST['email']}')";
+//execute		
+		$result = $dbc -> query($sql);
+//close it
+		$dbc-> close();
+//just in case 	
+		echo'ارسال شد ';
+
+
+
+?>
