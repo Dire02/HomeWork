225d9e3fc036059d841e55e3eda66423b4196040
Parsa <92381546+Dire02@users.noreply.github.com>
2021-11-15 23:40:07 +0330

    Add files via upload

--- register/register.php
+++ register/register.php
@@ -4,8 +4,8 @@
 //conect to db
 		$dbc = new mysqli ( 'localhost', 'root', '', 'Users' );
 //insert query
-		$sql = "INSERT INTO users (name,pass,gender,email)
-				VALUES('{$_POST['name']}', '{$_POST['pass']}', '{$_POST['gender']}', '{$_POST['email']}')";
+		$sql = "INSERT INTO users (name,pass,email)
+				VALUES('{$_POST['name']}', '{$_POST['pass']}','{$_POST['email']}')";
 //execute		
 		$result = $dbc -> query($sql);
 //close it
