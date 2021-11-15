718497157bbb0164a1ed284bc5d2f71ca5595d56
Parsa <92381546+Dire02@users.noreply.github.com>
2021-11-16 02:29:58 +0330

    Add files via upload

---
+++ editProduct/editProduct.php
@@ -0,0 +1,18 @@
+<?php
+// var_dump( $_POST );
+
+//conect to db
+	//	$dbc = new mysqli ( 'localhost', 'root', '', 'product' );
+//insert query
+	//	$sql = "INSERT INTO product (title,duration,discription,price)
+	//			VALUES('{$_POST['title']}', '{$_POST['duration']}', '{$_POST['discription']}', '{$_POST['price']}')";
+//execute		
+	//	$result = $dbc -> query($sql);
+//close it
+	//	$dbc-> close();
+//just in case 	
+	//	echo'ارسال شد ';
+
+
+
+?>
